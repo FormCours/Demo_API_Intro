d00b3447a9b8e72661680465c5b2530041907ec9
Aurélien Strimelle <aurelien.strimelle@bstorm.be>
2021-02-22 10:50:07 +0100

    Add controller and service data for category

---
+++ Demo_API_Intro/Controllers/CategoryController.cs
@@ -0,0 +1,76 @@
+﻿using Demo_API_Intro.Models;
+using Demo_API_Intro.ServiceData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Demo_API_Intro.Controllers
+{
+    public class CategoryController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult FindAll()
+        {
+            IEnumerable<Category> categories = CategoryService.Instance.GetAll();
+
+            return Json(categories);
+        }
+
+
+        [HttpGet]
+        public IHttpActionResult Find(int id)
+        {
+            Category category = CategoryService.Instance.Get(id);
+
+            if(category == null)
+            {
+                return NotFound();
+            }
+            return Json(category);
+        }
+
+
+        [HttpPost]
+        public IHttpActionResult InsertCategory(Category category)
+        {
+            Category categoryAlreadyExists = CategoryService.Instance.GetAll()
+                .FirstOrDefault(c => c.Name.Trim().ToLower() == category.Name.Trim().ToLower());
+
+            if(categoryAlreadyExists != null)
+            {
+                return BadRequest($"Category {category.Name} already exists!");
+            }
+
+            Category categorySave = CategoryService.Instance.Insert(category);
+            return Json(categorySave);
+        }
+
+
+        [HttpPut, HttpPatch]
+        public IHttpActionResult UpdateCategory(int id, Category category)
+        {
+            if(CategoryService.Instance.Get(id) == null)
+            {
+                return NotFound();
+            }
+
+            CategoryService.Instance.Update(id, category);
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+
+        [HttpDelete] 
+        public IHttpActionResult RemoveCategory(int id)
+        {
+            bool isOk = CategoryService.Instance.Delete(id);
+
+            if (isOk)
+                return Ok();
+
+            return NotFound();
+        }
+    }
+}
---
+++ Demo_API_Intro/ServiceData/CategoryService.cs
@@ -0,0 +1,74 @@
+﻿using Demo_API_Intro.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Demo_API_Intro.ServiceData
+{
+    public class CategoryService
+    {
+        #region Singleton
+        private static Lazy<CategoryService> _Instance = new Lazy<CategoryService>(() => new CategoryService());
+        public static CategoryService Instance { get { return _Instance.Value; } }
+        #endregion
+
+        #region Fake Data
+        private static List<Category> Categories = new List<Category>()
+        {
+            new Category() {Id = 1, Name = "Triple"},
+            new Category() {Id = 2, Name = "Spécial"},
+            new Category() {Id = 3, Name = "Trapiste"},
+            new Category() {Id = 4, Name = "Stout"},
+            new Category() {Id = 5, Name = "Pils"},
+            new Category() {Id = 6, Name = "Abbaye"},
+            new Category() {Id = 7, Name = "Fruité"},
+            new Category() {Id = 8, Name = "Blanche"},
+            new Category() {Id = 9, Name = "Pale lager"},
+        };
+        #endregion
+
+        private CategoryService()
+        { }
+
+        #region Crud
+        public IEnumerable<Category> GetAll()
+        {
+            return Categories.AsEnumerable();
+        }
+
+        public Category Get(int id)
+        {
+            return Categories.SingleOrDefault(c => c.Id == id);
+        }
+
+        public Category Insert(Category category)
+        {
+            int newId = Categories.Max(c => c.Id) + 1;
+            category.Id = newId;
+
+            Categories.Add(category);
+            return category;
+        }
+
+        public bool Update(int id, Category category)
+        {
+            Category target = Categories.SingleOrDefault(c => c.Id == id);
+
+            if(target != null)
+            {
+                target.Name = category.Name;
+                return true;
+            }
+            return false;
+        }
+
+        public bool Delete(int id)
+        {
+            int nbRow = Categories.RemoveAll(c => c.Id == id);
+
+            return nbRow == 1;
+        }
+        #endregion
+    }
+}
